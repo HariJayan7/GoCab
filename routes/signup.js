a34a1dd9a91faec91fbf45306f8cb1cff49ffa67
Kausti <doraemon152003102@gmail.com>
2023-10-15 16:37:10 +0530

    testing out

--- routes/signup.js
+++ routes/signup.js
@@ -7,6 +7,23 @@ function my_get(req,res,next)
     res.sendFile(path.join(__dirname,'../views/signup.html'));
 }
 
+function my_post(req,res,next)
+{
+    var body=req.body;
+    console.log(body);
+    var name=body.name;
+    var rollno=body.rollno;
+    var email=body.email;
+    var phno=body.phno;
+    var age=body.age;
+    var gender=body.gender;
+
+    //sql query here
+    res.redirect("/login");
+}
+
+
 router.get('/', my_get);
+router.post("/",my_post);
 
 module.exports = router;
