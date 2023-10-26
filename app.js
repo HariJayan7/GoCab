1da160abecc8c0b611ffaff7f1df029272289567
Kausti <doraemon152003102@gmail.com>
2023-10-26 21:53:33 +0530

    added database succesfully

--- app.js
+++ app.js
@@ -37,7 +37,77 @@ app.use("/searchResult",searchresultRouter);
 
 
 //this is for finding css and js
-app.use(express.static(__dirname + '/public'));
+// app.use(express.static(__dirname + '/public'));
+
+//database integration
+const sqlite3 = require('sqlite3').verbose();
+
+const db = new sqlite3.Database('./my_database.db');
+function initialize()
+{
+  function alreadyexists(err)
+  {
+    console.log("already initialized");
+  }
+  var createpersontable="CREATE TABLE person (\
+    pid varchar NOT NULL,\
+    name varchar NOT NULL,\
+    email varchar NOT NULL,\
+    phone varchar NOT NULL,\
+    age int NOT NULL,\
+    gender varchar NOT NULL,\
+    PRIMARY KEY (pid)\
+  );"
+
+  var createbookingtable="CREATE TABLE booking (\
+    bid int NOT NULL,\
+    etd DATETIME NOT NULL,\
+    start_dest varchar NOT NULL,\
+    final_dest varchar NOT NULL,\
+    cab_type varchar NOT NULL,\
+    cur_num int NOT NULL,\
+    max_num int NOT NULL,\
+    PRIMARY KEY (bid)\
+  );"
+
+  var createlistingstable="CREATE TABLE listings (\
+    bid int NOT NULL,\
+    pid varchar NOT NULL\
+    FOREIGN KEY(bid) REFERENCES booking(bid)\
+    FOREIGN KEY(pid) REFERENCES person(pid)\
+  );"
+
+  createlistingstable="CREATE TABLE `listings` (\
+    bid int NOT NULL,\
+    pid varchar NOT NULL\
+  );"
+  
+  var constrant1="ALTER TABLE `listings` ADD CONSTRAINT `listings_fk0` FOREIGN KEY (`bid`) REFERENCES `booking`(`bid`);"
+  var constrant2="ALTER TABLE `listings` ADD CONSTRAINT `listings_fk1` FOREIGN KEY (`pid`) REFERENCES `person`(`pid`);"
+
+  //running the commands here
+  db.run(createpersontable,alreadyexists);
+  db.run(createbookingtable,alreadyexists);
+  db.run(createlistingstable,alreadyexists);
+  // db.run(constrant1);
+  // db.run(constrant2);
+
+  const stmt = db.prepare('INSERT INTO person VALUES (?,?,?,?,?,?)');
+  stmt.run("0","root","root@gmail.com","000","0","M",alreadyexists);
+  stmt.run("1","rooti","rooti@gmail.com","0000","0","F",alreadyexists);
+  stmt.finalize();
+
+  function display(err,result)
+  {
+    console.log(result);
+  }
+  db.all('SELECT * FROM person', display);
+}
+
+
+db.serialize(initialize);
+db.close();
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
