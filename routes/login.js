f661fe0c586079ba7934a2f0517b49c4ab87cb86
Kausti <doraemon152003102@gmail.com>
2023-10-31 15:34:11 +0530

    added unbook functionality

--- routes/login.js
+++ routes/login.js
@@ -3,51 +3,35 @@ const path = require('path');
 var router = express.Router();
 const sqlite3 = require('sqlite3').verbose()
 const db = new sqlite3.Database('./my_database.db')
-
-
 function my_get(req,res,next)
 {
     res.sendFile(path.join(__dirname,'../views/login.html'));
 }
-
-function alreadyexists(err)
-{
-    console.log("already exists"+err);
-}
-
-
 function my_post(req,res,next)
 {
     var body=req.body;
     var username=body.username;
     var password=body.password;
-    function display(err,result)
+    function check_credentials(err,result)
     {
-
         if(err)
         {
-            console.log("some error"+err);
-        }
-        if(result==null)
-        {
-            console.log("result is null.");
-            res.redirect("/login");
+            console.log("GOT AN ERROR WHILE LOGGING IN "+err);
         }
         else
         {
-            // console.log(result);
             if(result[0]!=null && result[0].password==password)
             res.redirect("/dashboard");
             else
             {
-                console.log("username not found");
+                console.log("Username not found");
                 res.redirect("/login");
             }
         }
         
     }
 
-    db.all('SELECT * FROM person WHERE pid=?',username, display);
+    db.all('SELECT * FROM person WHERE pid=?',username, check_credentials);
     
 }
 
