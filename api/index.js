755002342506eb8c60bfde56b4237a463a8d179e
HariJayan7 <harinarayanan_b200741cs@nitc.ac.in>
2023-11-03 21:22:18 +0530

    vercel deployment again

--- api/index.js
+++ api/index.js
@@ -1,11 +1,3 @@
-var express = require("express");
-const path = require("path");
-var router = express.Router();
+import app from '../app.js';
 
-function my_get(req, res, next) {
-  res.sendFile(path.join(__dirname, "../views/index.html"));
-}
-
-router.get("/", my_get);
-
-module.exports = router;
+export default app;
